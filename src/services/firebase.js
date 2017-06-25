e435b6761b5f49ee2364ff2c81770f83dda0ea61
topisark <topi.sarkkinen@gmail.com>
2017-06-25 23:59:49 +0300

    Fixed STUFF

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,17 +2,15 @@ import firebase from 'firebase'
 import { cleanupMessage, getEmojiList } from '../utils'
 
 const config = {
-  '🔥': {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  }
 }
 
-if ( !firebase.apps.length ) firebase.initializeApp(config['🔥'])
+if ( !firebase.apps.length ) firebase.initializeApp(config)
 
 export default firebase
 
