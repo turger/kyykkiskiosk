7c86595442fc1fe6a70195fc2e51278b054dafc3
topisark <topi.sarkkinen@gmail.com>
2017-06-30 00:31:15 +0300

    Styling

--- src/Weather.js
+++ src/Weather.js
@@ -8,8 +8,8 @@ class Weather extends Component {
 
     return (
       <div className="Weather">
-        <div className="latest">
-          <div style={{"margin-bottom": "20px"}}>Nyt</div>
+        <div className="latest" style={{"font-size": "45px"}}>
+          <div style={{"margin-bottom": "10px", "font-size": "25px", "text-align": "center"}}>Nyt</div>
           <div>{ weatherData.latestTemp }°</div>
         </div>
         <table className="forecast" dangerouslySetInnerHTML={{__html: weatherData.forecast}}/>
