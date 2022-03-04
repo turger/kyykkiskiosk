1c1c4464ee2a7d860bd32e8fe2342352eeba82fa
topisark <topi.sarkkinen@gmail.com>
2022-03-04 12:16:04 +0200

    Night icons

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+import { getSunrise, getSunset } from 'sunrise-sunset-js'
 import Emoji from './Emoji'
+import nightWeatherEmojis from './nightWeatherEmojis'
 import weatherEmojis from './weatherEmojis'
 import './Weather.css'
 import { getFmiWeatherData, getLatestTemp } from './Requests'
 import { parseXmlWeatherData, formatTime } from './utils/utils'
+
+const HELSINKI_LAT = 60.192059
+const HELSINKI_LNG = 24.945831
+
+const isNight = date => {
+  const sunrise = getSunrise(HELSINKI_LAT, HELSINKI_LNG, new Date(date)).toISOString()
+  const sunset = getSunset(HELSINKI_LAT, HELSINKI_LNG, new Date(date)).toISOString()
+
+  return date < sunrise || date > sunset
+}
 
 class Weather extends Component {
   constructor(props) {
@@ -45,13 +57,15 @@ class Weather extends Component {
     }
   }
 
-  chooseIcon(temp, icon) {
+  chooseIcon(temp, icon, time) {
+    const night = isNight(time)
+
     if (temp > 20 && icon === 1) {
       return ":sun_with_face:"
     } else if (temp <= -10 && icon >= 41 && icon <= 53) {
       return ":snowman2:"
     } else {
-      return weatherEmojis[icon] || ':poop:'
+      return (night && nightWeatherEmojis[icon]) || weatherEmojis[icon] || ':poop:'
     }
   }
 
@@ -61,7 +75,7 @@ class Weather extends Component {
       <div className="Weather__item__box" key={weather.time}>
         <div className="Weather__item__time">{ formatTime(weather.time)}</div>
         <div className="Weather__item__temp">{ Math.round(weather.temperature) }°</div>
-        <Emoji name={ this.chooseIcon(Math.round(weather.temperature), weather.weathersymbol3) }/>
+        <Emoji name={ this.chooseIcon(Math.round(weather.temperature), weather.weathersymbol3, weather.time) }/>
         <div className="Weather__item__wind" key={key}>
           <div className="Weather__item__wind__ms"><span className="Weather__item__wind__value">{ Math.round(weather.windspeedms) }</span></div>
         </div>
@@ -83,8 +97,8 @@ class Weather extends Component {
              </div>
            }
            { forecast
-              .slice(1, 20)
               .filter(item => item.time > new Date().toISOString())
+              .slice(1, 20)
               .filter((w, key) => key % 3 === 0)
               .slice(0, 5)
               .map(weather => this.renderWeatherItem(weather))
---
+++ src/nightWeatherEmojis.js
@@ -0,0 +1,6 @@
+export default {
+  1: ':full_moon:', //'selkeää'
+  2: ':cloud:', //'puolipilvistä'
+  22: ':cloud_with_rain:', //'sadekuuroja'
+  71: ':cloud_with_rain:', //'heikkoja räntäkuuroja'
+}
